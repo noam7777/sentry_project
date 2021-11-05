928f57587df15dcc1656879363de748d16f1dde1
noam <noam@robotican.co>
2021-11-05 08:56:01 +0200

    cleanning

--- test_gimbal_sucsessfull_servos/test_serial.py
+++
@@ -1,9 +0,0 @@
-import serial
-import time
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB1',9600, timeout=1)
-    ser.flush()
-    
-    while True:
-        ser.write(b"1-131-113-1-1\n")
-        time.sleep(1)
